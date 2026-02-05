4a99358bba2dbd140b91873da81c10f4510237e4
Nischay <nischaybagari@gmail.com>
2026-02-05 16:06:51 +0530

    add

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,8 +2,10 @@ import Hero from '@/components/home/Hero';
 import IconCtaSection from '@/components/home/IconCtaSection';
 import ExploreLoginCta from '@/components/home/ExploreLoginCta';
 import NewsSection from '@/components/home/NewsSection';
-import BenefitsSection from '@/components/home/BenefitsSection';
 import GivingSection from '@/components/home/GivingSection';
+import WhereWeWorkSection from '@/components/home/WhereWeWorkSection';
+import AlumniDirectorySection from '@/components/home/AlumniDirectorySection';
+import NotableAlumniSection from '@/components/home/NotableAlumniSection';
 
 const HomePage = () => {
   return (
@@ -11,8 +13,10 @@ const HomePage = () => {
       <Hero />
       <ExploreLoginCta />
       <IconCtaSection />
+      <WhereWeWorkSection />
+      <NotableAlumniSection />
+      <AlumniDirectorySection />
       <NewsSection />
-      <BenefitsSection />
       <GivingSection />
     </main>
   );
